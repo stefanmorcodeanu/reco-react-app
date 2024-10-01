84525b038126ff1e57e4a0ebb366e6ad82b47e71
stefanmorcodeanusonos <stefan.morcodeanu@sonos.com>
2024-10-01 14:07:57 +0300

    feat: add exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,147 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useMemo, useState} from 'react'
 import './App.css'
 
+import {DataGrid, GridColDef} from '@mui/x-data-grid';
+import {Drawer} from "@mui/material";
+
+const backendUrl = '/api/v1/app-service/get-apps'
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+    const [tableData, setTableData] = useState([]);
+    const [pageSize, setpageSize] = useState(50);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [drawerState, setDrawerState] = useState(false);
+    const [activeApp, setActiveApp] = useState<{ appName: string, category: string, appUsers: Array<string>} | null>(null);
+
+    useEffect(() => {
+        fetch(backendUrl, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'  // Set the Content-Type to application/json
+            },
+            body: JSON.stringify({
+                "pageNumber": pageNumber,
+                "pageSize": pageSize
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data, 'DATA');
+                setTableData(data.appRows);
+            })
+            .catch(err => console.error("Error:", err));
+    },[pageNumber, pageSize]);
+
+
+    const rows = useMemo(() => {
+        if(!tableData?.length) return []
+
+        return tableData.map((row: {
+            appId: string
+        }) => ({
+            ...row,
+            id: row?.appId,
+            connector: 'Reco'
+        }))
+    }, [tableData])
+
+
+    const columns: GridColDef[] = [
+        { field: 'appName', headerName: 'Name', width: 250 },
+        { field: 'category', headerName: 'Category', width: 250 },
+        { field: 'connector', headerName: 'Connector', width: 250 },
+    ];
+
+    const openDrawer = (appId) => {
+        setDrawerState(true);
+
+        fetch(`/api/v1/app-service/get-app-overview/${appId}`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'ngrok-skip-browser-warning': '69420'
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                setActiveApp(data.appOverview)
+            })
+            .catch(err => console.error("Error:", err));
+
+        fetch(`/api/v1/app-service/get-app-overview-users/${appId}`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'ngrok-skip-browser-warning': '69420'
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data, 'DATA');
+                setActiveApp((prevState) => ({
+                    ...prevState,
+                    appUsers: [...(data.appUsers || [])]
+                }))
+            })
+            .catch(err => console.error("Error:", err));
+
+
+    }
+
+    const closeDrawer = () => {
+        setDrawerState(false)
+        setActiveApp(null)
+    }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ height: 600, width: '100%' }}>
+        <DataGrid
+            initialState={{
+                sorting: {
+                    sortModel: [{ field: 'appName', sort: 'asc' }],
+                },
+                pagination: { paginationModel: { pageSize: 25 } },
+            }}
+            rows={rows}
+            columns={columns}
+            pageSizeOptions={[25, 50]}
+
+            onRowClick={(row) => openDrawer(row.id)}
+        />
+
+
+        <Drawer open={drawerState} onClose={closeDrawer} anchor={'right'}>
+            <div style={{ padding: 20 }}>
+                <h3>App overview</h3>
+                {activeApp && (
+                    <>
+                        <h4>{activeApp.appName}</h4>
+                        <div style={{ border: '1px solid black' , borderRadius: '5px', padding: 12}}>
+                            <h5>App Name: {activeApp.appName} </h5>
+                            <h5>Category: {activeApp.category} </h5>
+                            <h5>Connector: Reco </h5>
+                        </div>
+                    </>
+                )}
+
+                {!!activeApp?.appUsers?.length && (
+                    <>
+                        <h3>usernames</h3>
+                        <ul>
+                            {activeApp.appUsers.map(user=> (
+                                <li>{user}</li>
+                            ))}
+                        </ul>
+
+                    </>
+
+                )}
+            </div>
+
+        </Drawer>
+    </div>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy requests starting with /api to the backend server at localhost:5000
+      '/api': {
+        target: 'https://f791-2a02-908-2540-80e0-21b3-2fbc-fb2c-3b13.ngrok-free.app', // Backend server URL
+        changeOrigin: true,  // Needed for virtual hosted sites
+      }
+    }
+  }
 })
